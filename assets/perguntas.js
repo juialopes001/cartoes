96367c5ba60085fda456c516a287b13fe2ce5963
juialopes001 <lopes.mendes.julia@escola.pr.gov.br>
2024-10-22 20:06:38 -0300

    Add files via upload

---
+++ assets/perguntas.js
@@ -0,0 +1,35 @@
+criaCartao (
+   'História',
+   'Quem descobriu o Brasil?',
+   'Pedro Álvares Cabral'
+)
+
+criaCartao (
+   'Português',
+   'O que é uma vogal?',
+   'É uma letra do alfabeto'
+)
+
+criaCartao (
+   'Geografia',
+   'Qual a capital Da França?',
+   'A capital da França é Paris'
+)
+
+criaCartao (
+    'Matemática',
+    'Quanto é 53+62?',
+    'É 115 no total'
+)
+
+criaCartao (
+    'Projeto de vida',
+    'Quais são os 4 elementos do Projeto de vida?',
+    'Definição de uma visão de futuro, sonhos e compromisso'
+)
+
+criaCartao (
+    'Inglês',
+    'Como se diz oi em inglês?',
+    'Se diz HI (Rai)'
+)
